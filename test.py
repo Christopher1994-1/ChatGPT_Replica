f211989d99f79f65038e2d0b453cf0891a7c4aeb
CEJ Van Niekirk <cejvanniekirk@gmail.com>
2025-01-20 22:15:54 -0800

    up

--- test.py
+++ test.py
@@ -2,3 +2,42 @@ import requests
 import json
 import os
 
+
+
+
+#. MAIN FUNCTION FOR API CALL
+def main_api_call(promot: str) -> str:
+
+    openai_api_key = os.environ['OPENAI_API_KEY']
+
+    url = "https://api.openai.com/v1/chat/completions"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai_api_key}"
+    }
+
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "system",
+                "content": "you are a helpful assistant"
+            },
+            {
+                "role": "user",
+                "content": promot
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        return str(response.json()['choices'][0]['message']['content'])
+    else:
+        print("Error:", response.status_code, response.text)
+        return ''
+
+
